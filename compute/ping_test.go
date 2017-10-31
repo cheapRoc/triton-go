c199beaa49d439c1480aaf6b1229269b0344e733
Justin Reagor <justin.reagor@joyent.com>
2017-10-31 15:11:35 -0400

    WIP: Got ping unit tests functional

--- compute/ping_test.go
+++ compute/ping_test.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"io/ioutil"
 	"net/http"
+	"reflect"
 	"strings"
 	"testing"
 
+	"github.com/joyent/triton-go/authentication"
 	"github.com/joyent/triton-go/client"
 	"github.com/joyent/triton-go/compute"
 	"github.com/joyent/triton-go/testutils"
@@ -17,24 +19,28 @@ import (
 // test borked 410 response (TritonError)
 // test bad JSON decode
 
-func TestPing(t *testing.T) {
+func buildClient() *compute.ComputeClient {
+	testSigner, _ := authentication.NewTestSigner()
 	httpClient := &client.Client{
+		Authorizers: []authentication.Signer{testSigner},
 		HTTPClient: &http.Client{
-			Transport: http.DefaultTransport,
+			Transport: testutils.DefaultMockTransport,
 			CheckRedirect: func(_ *http.Request, _ []*http.Request) error {
 				return http.ErrUseLastResponse
 			},
 		},
 	}
-
-	c := &compute.ComputeClient{
+	return &compute.ComputeClient{
 		Client: httpClient,
 	}
+}
+
+func TestPing(t *testing.T) {
+	computeClient := buildClient()
 
 	do := func(ctx context.Context, pc *compute.ComputeClient) (*compute.PingOutput, error) {
-		testutils.ActivateClient(false)
-
 		defer testutils.DeactivateClient()
+
 		ping, err := pc.Ping(ctx)
 		if err != nil {
 			return nil, err
@@ -44,20 +50,34 @@ func TestPing(t *testing.T) {
 
 	t.Run("successful", func(t *testing.T) {
 		testutils.RegisterResponder("GET", "/--ping", func(req *http.Request) (*http.Response, error) {
-			body := strings.NewReader(`{ 'ping': 'pong' }`)
+			body := strings.NewReader(`{
+	"ping": "pong",
+	"cloudapi": {
+		"versions": ["7.0.0", "7.1.0", "7.2.0", "7.3.0", "8.0.0"]
+	}
+}`)
+			header := http.Header{}
+			header.Add("Content-Type", "application/json")
+
 			return &http.Response{
 				StatusCode: 200,
+				Header:     header,
 				Body:       ioutil.NopCloser(body),
 			}, nil
 		})
 
-		ctx := context.Background()
-		resp, err := do(ctx, c)
-		if err == nil {
+		resp, err := do(context.Background(), computeClient)
+		if err != nil {
 			t.Fatal(err)
 		}
+
 		if resp.Ping != "pong" {
-			t.Error("ping was not pong: expected %s", resp.Ping)
+			t.Errorf("ping was not pong: expected %s", resp.Ping)
+		}
+
+		versions := []string{"7.0.0", "7.1.0", "7.2.0", "7.3.0", "8.0.0"}
+		if !reflect.DeepEqual(resp.CloudAPI.Versions, versions) {
+			t.Errorf("ping did not contain CloudAPI versions: expected %s", versions)
 		}
 	})
 
